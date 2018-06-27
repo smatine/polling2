1712815d0f2860542e0feafb8e966d77001bc61e
smatine <soufiane.matine@gmail.com>
2018-06-28 01:47:36 +0200

    ok

---
+++ src/main/java/com/example/polls/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.example.polls.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Autowired
+	RequestInterceptor requestInterceptor;
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+    
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestInterceptor);
+	}
+	
+}
