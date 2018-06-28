bae838fb2f202d68de647ad0e145ce32d405ae4e
smatine <soufiane.matine@gmail.com>
2018-06-28 02:05:41 +0200

    Update Choice.java

--- src/main/java/com/example/polls/model/Choice.java
+++ src/main/java/com/example/polls/model/Choice.java
@@ -5,9 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
-/**
- * Created by rajeevkumarsingh on 20/11/17.
- */
+
 
 @Entity
 @Table(name = "choices")
