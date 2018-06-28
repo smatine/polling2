e17435f29f9f5accbb7ee8160fa35a75aaa5c809
smatine <soufiane.matine@gmail.com>
2018-06-28 02:03:17 +0200

    Update WebMvcConfig.java

--- src/main/java/com/example/polls/config/WebMvcConfig.java
+++ src/main/java/com/example/polls/config/WebMvcConfig.java
@@ -6,9 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Created by rajeevkumarsingh on 02/08/17.
- */
+
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
